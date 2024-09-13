9f23797a7561f55f9d293aa8ade0b31efec55121
Muhtasim <muhtasimrahman810@gmail.com>
2024-09-13 14:22:57 +0600

    add bank.java

---
+++ src/application/Bank.java
@@ -0,0 +1,13 @@
+package banking;
+
+public class Bank {
+    private String name;
+
+    public Bank(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
